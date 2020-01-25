bce21d9b3cf112452e46df9cc963b47bc073fe5d
Muhammed Essa <muhammed.essa@gmail.com>
2020-01-25 08:05:53 +0300

    done

---
+++ My App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My App
+//
+//  Created by Muhammed Essa on 1/17/20.
+//  Copyright © 2020 Muhammed Essa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
